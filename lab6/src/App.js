ef96320945322aae4e67f1cc3498f98f4b0d058b
rmn20 <romax9lahin@gmail.com>
2022-04-19 19:07:11 +0300

    lab6

---
+++ lab6/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Element1} from './elements/Element1';
+import {Element2} from './elements/Element2';
+
+function App() {
+  return (
+    <div className="App">
+      <Element1 />
+      <Element2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ lab6/src/elements/Element2.jsx
@@ -0,0 +1,5 @@
+export const Element2 = () => {
+    return (<div>
+                Function element
+            </div>)
+}
